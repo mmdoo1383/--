b400fc09fda8540a60094a987acd456441ee148a
mmdoo1383 <sobhany9904@gmail.com>
2025-01-18 23:41:10 +0330

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // مرحله 1: نمایش پیام برای دریافت نام کاربر
+        Console.Write("enter your name: ");
+        string name = Console.ReadLine(); // دریافت نام کاربر و ذخیره در متغیر name
+
+        // مرحله 2: نمایش پیام برای دریافت شماره تلفن کاربر
+        Console.Write("enter your phone number: ");
+        string phoneNumber = Console.ReadLine(); // دریافت شماره تلفن کاربر و ذخیره در متغیر phoneNumber
+
+        // مرحله 3: ترکیب نام و شماره تلفن در یک متن
+        string output = $"name: {name}, phone number: {phoneNumber}";
+
+        // مرحله 4: نمایش نتیجه به کاربر
+        Console.WriteLine(output);
+
+        // مرحله 5: پایان برنامه
+    }
+}
